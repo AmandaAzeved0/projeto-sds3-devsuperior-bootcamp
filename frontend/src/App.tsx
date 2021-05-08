7eca729550001101e678c45147dc86f9ab6ee662
AmandaAzevedo0 <amanda.educacional@gmail.com>
2021-05-08 16:55:38 -0300

    bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+// import React from 'react';
 
 
 function App() {
   return (
     <div>
-        <h1>ola mundo</h1>
+        <h1 className="text-primary">ola mundo</h1>
     </div>
   );
 }
